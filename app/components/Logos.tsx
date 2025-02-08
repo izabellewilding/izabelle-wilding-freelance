58bdae1ffab99d850a40a2f8586a30788b418448
Izabelle wilding <izabellewilding@gmail.com>
2025-02-08 20:11:39 +0000

    full height hero section & logos bar

--- app/components/Logos.tsx
+++ app/components/Logos.tsx
@@ -3,28 +3,45 @@ import Image from "next/image";
 
 export default function Logos() {
   return (
-    <div className="w-full md:max-w-screen-lg m-auto py-4 px-6 flex flex-col md:flex-row items-center justify-between">
-      <Image
-        src="/logo.svg"
-        alt="Qvara Logo"
-        width={50}
-        height={50}
-        className="w-12 h-12 md:w-16 md:h-16"
-      />
-      <Image
-        src="/logo.svg"
-        alt="Qvara Logo"
-        width={50}
-        height={50}
-        className="w-12 h-12 md:w-16 md:h-16"
-      />
-      <Image
-        src="/logo.svg"
-        alt="Qvara Logo"
-        width={50}
-        height={50}
-        className="w-12 h-12 md:w-16 md:h-16"
-      />
+    <div className="w-full bg-white">
+      <div className="w-full md:max-w-screen-xl m-auto py-6 px-6 flex flex-col md:flex-row items-center justify-between">
+        <Image
+          src="/figma.svg"
+          alt="Figma Logo"
+          width={50}
+          height={50}
+          className="w-12 h-12 md:w-24 md:h-24"
+        />
+        <Image
+          src="/figma.svg"
+          alt="Figma Logo"
+          width={50}
+          height={50}
+          className="w-12 h-12 md:w-24 md:h-24"
+        />
+        <Image
+          src="/react.svg"
+          alt="React Logo"
+          width={50}
+          height={50}
+          className="w-12 h-12 md:w-24 md:h-24"
+        />
+        <Image
+          src="/react.svg"
+          alt="React Logo"
+          width={50}
+          height={50}
+          className="w-12 h-12 md:w-24 md:h-24"
+        />
+
+        <Image
+          src="/logo.svg"
+          alt="Qvara Logo"
+          width={50}
+          height={50}
+          className="w-12 h-12 md:w-24 md:h-24"
+        />
+      </div>
     </div>
   );
 }

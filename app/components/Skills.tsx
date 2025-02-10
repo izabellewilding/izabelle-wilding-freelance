7ef630fbb390ea74aed7e124b84233db6106b469
Izabelle wilding <izabellewilding@gmail.com>
2025-02-10 15:02:50 +0000

    services section

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,59 +1,70 @@
-// import { Code, Palette, Smartphone, Zap } from "lucide-react";
 import Image from "next/image";
 
 const skills = [
   {
     name: "Web Design & Branding",
     icon: "/bulb.svg",
-    description:
-      "I can help you with your initial branding concepts right through to the final design of your website. My designs are always modern and fresh. I have created logos and branding for a number of clients.",
+    description: (
+      <>
+        I craft modern, fresh <strong>website designs</strong>—from initial
+        branding to final execution. Whether you need a <strong>logo</strong> or
+        a full <strong>brand identity</strong>, I’ve helped businesses create
+        standout visuals that leave a lasting impression.
+      </>
+    ),
   },
   {
     name: "Website Development",
     icon: "/code.svg",
-    description:
-      "The websites I create are always fully responsive (Web and Mobile friendly), super fast and SEO optimized. I use cutting edge technologies like React and NextJS to ensure that your website remains in the top tier of websites on the web. ",
-  },
-  {
-    name: "Mobile Development",
-    icon: "/bulb.svg",
-    description:
-      "I work alongside a React Native mobile developer to create mobile apps for both iOS and Android. I will create initial wireframes for your app and work with you to create a design that you love.",
+    description: (
+      <>
+        I build <strong>fast, fully responsive</strong>, and{" "}
+        <strong>SEO-optimized</strong> websites using{" "}
+        <strong>cutting-edge technologies</strong> like React and
+        Next.js—ensuring <strong>top-tier performance</strong> and a seamless
+        user experience on any device.
+      </>
+    ),
   },
   {
     name: "Performance Optimization",
-    icon: "/bulb.svg",
-    description: "Lighthouse, Web Vitals, SEO",
+    icon: "/optimise.svg",
+    description: (
+      <>
+        I optimize websites for{" "}
+        <strong>speed, accessibility, and performance</strong>, running in-depth
+        tests to ensure a smooth experience for users worldwide.
+      </>
+    ),
   },
 ];
 
 export default function Skills() {
   return (
-    <section id="skills" className="py-20 bg-white">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 text-center font-heading">
-          My Skills
+    <section id="skills" className="py-20">
+      <div className="container mx-auto px-4 max-w-screen-2xl">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 ">
+          Professional help with...
         </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {skills.map((skill) => (
             <div
               key={skill.name}
-              className="bg-white p-6 rounded-lg shadow-md flex justify-center items-center flex-col text-center"
+              className="bg-white p-6 rounded-lg flex flex-col space-y-4"
             >
-              <Image
-                src={skill.icon}
-                alt={skill.name}
-                width={50}
-                height={50}
-                className="h-20 w-20"
-              />
-              {/* <skill.icon className="w-12 h-12 text-indigo-600 mb-4" /> */}
-              <div>
-                <h3 className="text-xl font-semibold text-gray-800 mb-2 font-heading">
-                  {skill.name}
-                </h3>
-                <p className="text-gray-600 font-body">{skill.description}</p>
+              <div className="h-20 w-20 flex items-center justify-center">
+                <Image
+                  src={skill.icon}
+                  alt={skill.name}
+                  width={50}
+                  height={50}
+                  className="w-16 h-16 object-contain"
+                />
               </div>
+              <h3 className="text-2xl font-semibold text-gray-800">
+                {skill.name}
+              </h3>
+              <p className="text-gray-600">{skill.description}</p>
             </div>
           ))}
         </div>

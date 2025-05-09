ad85153606526ebd634e42ef4c0a497469909800
Izabelle wilding <izabellewilding@gmail.com>
2025-05-09 11:00:58 +0100

    hero styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,7 +10,7 @@ const headingPhrases = [
 
 export default function Hero() {
   return (
-    <section className="bg-[#f5f5f5] min-h-screen flex items-center px-6 md:px-20 py-20">
+    <section className="bg-[#f5f5f5] min-h-screen flex md:items-center justify-center pt-16 px-6 md:px-20 md:py-20">
       <div className="max-w-5xl">
         <div className="mb-8">
           {headingPhrases.map((text, index) => (
@@ -19,27 +19,30 @@ export default function Hero() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: index * 0.5, duration: 0.6 }}
-              className="text-4xl sm:text-5xl md:text-6xl font-bold text-black leading-tight tracking-tight"
+              className="text-4xl sm:text-5xl md:text-7xl font-bold text-black leading-tight tracking-tight"
             >
               {text}
             </motion.h1>
           ))}
         </div>
 
-        <motion.p
+        <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: headingPhrases.length * 0.5, duration: 0.8 }}
-          className="text-lg text-gray-800 max-w-xl mb-10"
+          className="text-lg text-gray-800  w-full flex justify-end mb-10"
         >
-          With six years of experience as a frontend developer, I’ve worked
-          across startups and mid-sized digital agencies to design and build
-          polished, accessible user interfaces. At Qvara, I led frontend efforts
-          on key projects like an option calculator, support site, and user
-          onboarding flows — shaping both UX and UI. Known for my attention to
-          detail, creativity, and collaborative mindset, I consistently bring
-          new ideas to the table.
-        </motion.p>
+          <p className="max-w-xl">
+            I bring over six years of experience in web design & development,
+            improving performance, UX, and building innovative products.
+            {/* across startups and mid-sized digital agencies to design and build
+            polished, accessible user interfaces. At Qvara, I led frontend
+            efforts on key projects like an option calculator, support site, and
+            user onboarding flows — shaping both UX and UI. Known for my
+            attention to detail, creativity, and collaborative mindset, I
+            consistently bring new ideas to the table. */}
+          </p>
+        </motion.div>
 
         <div className="flex gap-4">
           {/* <Button variant="primary" text="Work with me" />
